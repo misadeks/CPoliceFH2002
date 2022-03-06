72c2141cce8fea849f054ea2c97a5bbce2107b1e
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-03-06 11:36:46 +0100

    Extracted helper classes to separate files, removed redundant methods

--- IterativeSierpinskiDrawer.py
+++ IterativeSierpinskiDrawer.py
@@ -1,111 +1,80 @@
-import turtle
 import math
-
-from util import get_random_color
-
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get(self):
-        return self.x, self.y
-
-    @staticmethod
-    def midpoint(pos1, pos2):
-        return Position((pos1.x + pos2.x) // 2, (pos1.y + pos2.y) // 2)
+from colors import Colors
+from triangle import *
 
 
-class Triangle:
-    def __init__(self, bottom_left_v, bottom_right, top_v):
-        self.bottom_left_v = bottom_left_v
-        self.bottom_right_v = bottom_right
-        self.top_v = top_v
-
-    def get(self):
-        return self.bottom_left_v, self.bottom_right_v, self.top_v
-
-    def draw(self, color="white"):
-        t = turtle.Turtle(visible=False)
-        t.fillcolor(color)
-        t.speed(0)
-        t.up()
-        t.goto(*self.bottom_left_v.get())
-        t.down()
-        t.begin_fill()
-        t.goto(*self.bottom_right_v.get())
-        t.goto(*self.top_v.get())
-        t.goto(*self.bottom_left_v.get())
-        t.end_fill()
-
-"""
-    mode 0 - black & white
-    mode 1 - 3 colors
-    mode 2 - 4 colors
-    mode 3 - 5 random colors, mode 1
-    mode 4 - 5 random colors, mode 2
-"""
 class IterativeSierpinksiDrawer:
-    def __init__(self, screen, size, position, mode):
+    """
+        This class draws Sierpinski Triangle in increments on the turtle canvas.
+        Triangle is centered at the position passed to the constructor, and it can be drawn in several modes,
+        based on the provided argument;
+            mode=0 - black & white
+            mode=1 - 3 colors
+            mode=2 - 4 colors
+            mode=3 - 3 random colors, similar to mode 1
+            mode=4 - 3 random colors, similar to mode 2
+    """
+    def __init__(self, screen: type(turtle.Screen()), size: int, position: Position, mode: int):
         self.screen = screen
         self.size = size
-        self.position = Position(*position)
+        self.position = position
         self.level = 0
         self.triangles = []
         self.mode = mode
         if mode == 1:
-            # ovo je kada se u random color prosledjuje index
             self.colors = ["yellow", "orange", "red"]
         elif mode == 2:
-            # ovaj ide ako se u random_color prosledi current_level
             self.colors = ["lime", "yellow", "orange", "red"]
         elif mode in (3, 4):
-            self.colors = [get_random_color() for i in range(5)]
+            self.colors = Colors.random_pallet(mode)
         turtle.tracer(0, 0)
         turtle.colormode(255)
 
-    def draw_all(self, color="white"):
-        for triangle in self.triangles:
-            triangle.draw(color)
-
-    @staticmethod
-    def make_triangle(size, position):
-        h = math.floor(size * math.sqrt(3) / 2)
-        bottom_left_vertex = Position(position.x - size / 2, position.y - h // 3)
-        bottom_right_vertex = Position(position.x + size / 2, position.y - h // 3)
-        top_vertex = Position(position.x, position.y + 2 * h // 3)
-        return Triangle(bottom_left_vertex, bottom_right_vertex, top_vertex)
-
-    def pick_color(self, index):
-        v = 0
+    def __pick_color(self, index: int) -> str | tuple[int, int, int]:
+        """Depending on the drawing mode, picks the color for the next layer"""
+        ind = 0
         if self.mode in (1, 3):
-            v = index
+            ind = index
         elif self.mode in (2, 4):
-            v = self.level
-        return self.colors[v % len(self.colors)]
+            ind = self.level
+        return self.colors[ind % len(self.colors)]
 
-    def draw_next(self):
+    def draw_next(self) -> None:
+        """Calculates triangles to be drawn in order to create a new layer, then draws them"""
         if self.level == 0:
-            self.triangles.append(self.make_triangle(self.size, self.position))
-            self.draw_all("black")
+            # If first layer is being drawn, only draw an centered empty black triangle
+            x, y = self.position.get()
+            h = math.floor(self.size * math.sqrt(3) / 2)
+            bottom_left_vertex = Position(x - self.size // 2, y - h // 3)
+            bottom_right_vertex = Position(x + self.size // 2, y - h // 3)
+            top_vertex = Position(x, y + 2 * h // 3)
+            self.triangles.append(Triangle(bottom_left_vertex, bottom_right_vertex, top_vertex))
+            self.triangles[0].draw("black")
         else:
+            """For all other layers, new triangles should be generated
+            In each iteration one of triangles from previous layer is split in 4 parts,
+            and the middle part is deleted, while others are returned into the array"""
             new_triangles = []
             index = 0
             for triangle in self.triangles:
                 bottom_left_v, bottom_right_v, top_v = triangle.get()
-                bottom_center_v = Position.midpoint(bottom_left_v, bottom_right_v)
-                left_center_v = Position.midpoint(bottom_left_v, top_v)
-                right_center_v = Position.midpoint(top_v, bottom_right_v)
+                # First generate midpoints of all the sides of the triangle
+                bottom_center_v = triangle.base_midpoint()
+                left_center_v = triangle.left_side_midpoint()
+                right_center_v = triangle.right_size_midpoint()
+                # Create small triangles that are bottom left, bottom right and top parts of the original
                 new_triangles.append(Triangle(bottom_left_v, bottom_center_v, left_center_v))
                 new_triangles.append(Triangle(bottom_center_v, bottom_right_v, right_center_v))
                 new_triangles.append(Triangle(left_center_v, right_center_v, top_v))
+                # Based on color mode, color the inner part of the current triangle ( delete it)
                 if self.mode in range(1, 5):
-                    # color modes
-                    Triangle(left_center_v, right_center_v, bottom_center_v).draw(self.pick_color(index))
+                    # If triangle is drawn in color
+                    Triangle(left_center_v, right_center_v, bottom_center_v).draw(self.__pick_color(index))
                     index += 1
                 else:
-                    Triangle(left_center_v, right_center_v, bottom_center_v).draw() # black-white
+                    # If triangle is black and white
+                    Triangle(left_center_v, right_center_v, bottom_center_v).draw()
 
+            # Replace old triangles
             self.triangles = new_triangles
         self.level += 1
